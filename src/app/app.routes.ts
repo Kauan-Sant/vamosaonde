9790607f50a722c3a422fc7d029ec56e80b2c279
ksant <09kauanjesus@gmail.com>
2025-05-31 10:16:46 -0300

    feat: termos de uso adicionado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { BuscaComponent } from './components/busca/busca.component';
 import { ContatoComponent } from './components/contato/contato.component';
+import { TermosComponent } from './components/termos/termos.component';
 
 export const routes: Routes = [
     { path: 'cadastro', component: CadastroComponent },
@@ -13,6 +14,7 @@ export const routes: Routes = [
     { path: 'busca', component: BuscaComponent },
     { path: 'detalhes/:placeId', loadComponent: () => import('./components/detalhes/detalhes.component').then(m => m.DetalhesComponent) },
     { path: 'contato', component: ContatoComponent },
+    { path: 'termos', component: TermosComponent},
     { path: 'cadastro', component: ContatoComponent }
 
 ];

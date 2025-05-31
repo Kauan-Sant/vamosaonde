83314a0769df233a00ac5600dbd41194b65dc308
ksant <09kauanjesus@gmail.com>
2025-05-30 23:55:44 -0300

    chore: rotas das paginas e HttpClient e NgxMask no config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { BuscaComponent } from './components/busca/busca.component';
+import { ContatoComponent } from './components/contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'cadastro', component: CadastroComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'inicio', component: InicioComponent },
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+    { path: 'busca', component: BuscaComponent },
+    { path: 'detalhes/:placeId', loadComponent: () => import('./components/detalhes/detalhes.component').then(m => m.DetalhesComponent) },
+    { path: 'contato', component: ContatoComponent },
+    { path: 'cadastro', component: ContatoComponent }
+
+];
